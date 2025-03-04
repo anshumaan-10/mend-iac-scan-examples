c41ec383f7e239b6cc1c6af7266ab7e029487cd5
ANSHUMAAN SINGH <124035463+anshumaan-10@users.noreply.github.com>
2025-03-04 12:43:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region                      = var.region
   skip_credentials_validation = true
   skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
+  skip_metadata_api_check     = true 
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
 }
